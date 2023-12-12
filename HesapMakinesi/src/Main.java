bc806f96566dd382679901baff5e9bb363c0bcc5
Devran EROĞLU <devraneroglu@gmail.com>
2023-12-13 02:55:01 +0300

    Rev0
    Kullanıcıdan alınan iki sayının hangi aritmetik işlem yapılacağı bilgisine göre  swich-case kullanarak  sonucunların hesaplanması

---
+++ HesapMakinesi/src/Main.java
@@ -0,0 +1,39 @@
+/*
+Hesap makinesini uygulamasının switch-case ile yapılması
+Kullanıcıdan iki tane sayı alınarak; bu sayılarla hangi aritmetik işlemin yapılacağına göre
+ekrana sonucun yazdırılması
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      double sayi1 , sayi2 , sonuc ;
+      int islem;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("İlk sayiyi girin : ");
+        sayi1 = sc.nextDouble();
+        System.out.print("İkinci sayiyi girin : ");
+        sayi2 = sc.nextDouble();
+        System.out.println("Girilen iki sayı için hangi işlem yapılsın\n 1-Topla \n 2-Çıkar \n 3-Çarp \n 4-Böl");
+        islem = sc.nextInt();
+        switch (islem){
+          case 1:
+            sonuc = sayi1 + sayi2;
+            System.out.print("İşlem sonucu : " +sonuc);
+            break;
+          case 2:
+            sonuc = sayi1 - sayi2;
+            System.out.print("İşlem sonucu : " +sonuc);
+            break;
+          case 3:
+            sonuc = sayi1 * sayi2;
+            System.out.print("İşlem sonucu : " +sonuc);
+            break;
+          case 4:
+            sonuc = sayi1 / sayi2;
+            System.out.print("İşlem sonucu : " +sonuc);
+            break;
+          default:
+        }
+
+    }
+}
